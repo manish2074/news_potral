f60b5a418ef02640a12337fc9a0fd6d733a4dd67
manish2074 <manish.kharel2074@gmail.com>
2019-08-07 09:23:35 +0545

    Initial commit

---
+++ accounts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views import Views
+# Create your views here.
+
+class UserSignUpView(Views):
+    pass 
+    
---
+++ project/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+def home(request):
+    template_name="index.html"
+    return render(request, template_name)
