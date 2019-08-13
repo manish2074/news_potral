dd57a46848f6bed1271184559b7fa9e23bdff179
manish2074 <manish.kharel2074@gmail.com>
2019-08-13 09:47:48 +0545

    news_4

--- news/models.py
+++ news/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class News(models.Model):
     CATEGORY=(("0","Politics"),("1","Sports"),("2","Fashion"),("3","Technology"))
     title = models.CharField(max_length=250)
     story = models.TextField()
-    category= models.CharField(max_length=2)
+    category= models.CharField(choices=CATEGORY, max_length=2)
     slug=models.CharField(max_length=270)
+    reporter = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     cover_image= models.ImageField(upload_to="uploads")
--- news/views.py
+++ news/views.py
@@ -3,9 +3,25 @@ from django.views.generic import CreateView
 from news import forms
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.utils.text import slugify
+from django.urls import reverse_lazy
 # Create your views here.
 
 class CreateNewsView(LoginRequiredMixin,CreateView):
-    login_url = "/accounts/login"
+    login_url = "/accounts/laogin"
     form_class= forms.CreateNewsForm
     template_name = "news/create_news.html"
+    success_url=reverse_lazy('home')
+
+    def form_valid(self, form):
+        print("CLEANED_DATA:",form.cleaned_data)
+        title = form.cleaned_data['title']
+        news= form.save(commit=False)
+        news.reporter = self.request.user
+        news.slug = slugify(title)
+        news.save()
+        return super(CreateNewsView, self).form_valid(form)
+
+    def form_invalid(self,form):
+        print(form.errors)
+        return super (CreateNewsView, self).form_invalid(form)
